f74d9cabf04a36a7ac87b976d817edbfb8f32183
Mortexe <paris.chrysanthou@gmail.com>
2017-08-05 19:34:36 +0300

    1 input error
    fixed error when only 1 row of inputs is typed

--- js/input-addon.js
+++ js/input-addon.js
@@ -2,6 +2,7 @@ var markMap=[];
 var percMap=[];
 
 $( document ).ready(function() {
+  $('.result').addClass('hidden');
   let i = 0;
   while(i<3){
     $('.row-inputs').clone().appendTo('#data');
@@ -37,6 +38,16 @@ $('body').on('click', '#calc', function(){
     var i=1;
     var mark;
     var firstMark = ((percMap[0]/100)*markMap[0]);
+
+    if(percLenght && markLength==1){
+      console.log(firstMark,'total')
+      markMap=[]; // empty mark array
+      percMap=[]; // empty percentage array
+
+      document.getElementById('res').innerHTML = firstMark; //show result on HTML
+
+    }
+    else{
     while(i<markLength){
       if (i==1){
         marks = firstMark + ((percMap[i]/100)*markMap[i]); // calculate first and second marks only once
@@ -51,9 +62,12 @@ $('body').on('click', '#calc', function(){
       percMap=[]; // empty percentage array
 
       document.getElementById('res').innerHTML = marks; //show result on HTML
+    }
+      if($('.result').hasClass('hidden')){
+        $('.result').removeClass('hidden').addClass('visible');
+      }
   }
 });
-
 
 $('body').on('click','#add', function (){
   $('#data').append('<div class="col-xs-3"><input type="text" class="form-control mark" name="mark" placeholder="0 - 100"></div><div class="col-xs-3"><input type="text" class="form-control pc" name="pc" placeholder="%"></div><div class="col-xs-6"><input type="text" class="form-control tag" name="ref" placeholder=""></div></div>');
